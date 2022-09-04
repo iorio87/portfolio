699791b4a5469ec2e23abd521328cfe059b68c68
Luciano Petrate <lucianopetrate@gmail.com>
2022-09-04 12:34:13 -0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Hi! I'm Luciano, passionate about programming, investments and above
+          all for learning new things all the time. Since I was a kid I've
+          always been interested in programming and I always did it as a hobby
+          in my free time, but in 2020 I decided that I wanted to do it
+          professionally and dedicate myself 100% to what I really like so I
+          started a technical degree in Systems and in 2022 the FullStack
+          Developer bootcamp here at SoyHenry. In addition to what I learned at
+          SoyHenry I have knowledge in Python and Java.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          More about me... I have a bachelor's degree in administration and I
+          have been working in the treasury area for 15 years. I'm also an
+          enthusiast of new technologies focusing in the last 5 years in the
+          crypto world both from development (Blockchains, NFT, NFTGames, DAOs)
+          and investments (cryptocurrencies, Defi).
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
