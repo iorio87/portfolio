c6b64c8da0628f99deefd94e1d9c55c286146601
Luciano Petrate <lucianopetrate@gmail.com>
2022-10-20 23:56:01 -0300

    images change

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
 import mongo from "../assets/mongo.png";
-import graphql from "../assets/graphql.png";
+import graphql from "../assets/graphql.webp";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
 
